7b51cfdc0e283dd34e961364d61e22e2daf97087
Kartik <kartikkannan22@gmail.com>
2026-01-02 23:00:25 +0530

    ui improvements, audio, redefine word

--- assets/ui.tsx
+++ assets/ui.tsx
@@ -1,5 +1,8 @@
 import "@/assets/tailwind.css";
 import React from "react";
+import restart from "../assets/restart.svg";
+import cross from "../assets/cross.svg";
+import audio from "../assets/audio.svg";
 
 type PopupProps = {
   word: string;
@@ -9,8 +12,55 @@ type PopupProps = {
   handleClickOutside: () => void;
 };
 const Popup: React.FC<PopupProps> = ({ word, definition, mouseX, mouseY, handleClickOutside }) => {
+    const [isDragging, setIsDragging] = useState(false);
     const [positionX, setPositionX] = useState(mouseX);
     const [positionY, setPositionY] = useState(mouseY);
+    const [offsetX, setOffsetX] = useState(0);
+    const [offsetY, setOffsetY] = useState(0);
+    const [baseUrl, setBaseUrl] = useState('https://api.dictionaryapi.dev/api/v2/entries/en/');
+    const [audioUrl, setAudioUrl] = useState('');
+    const audioRef = useRef<HTMLAudioElement>(null);
+    useEffect(() => {
+        fetch(`${baseUrl}${word}`)
+        .then(response => response.json())
+        .then(data => {
+            setAudioUrl(data[0].phonetics[0].audio);
+        })
+        .catch(error => {
+            console.error('Error fetching audio', error);
+        });
+    }, [word]);
+
+    const handleMouseDown = (e: React.MouseEvent) => {
+        console.log('MOUSEDOWN');
+        setIsDragging(true);
+        // Calculate where in the element you clicked
+        const rect = e.currentTarget.getBoundingClientRect();
+        setOffsetX(e.clientX - rect.left);
+        setOffsetY(e.clientY - rect.top);
+    };
+
+    useEffect(() => {
+        if (!isDragging) return;
+
+        const handleMouseMove = (e: MouseEvent) => {
+            setPositionX(e.clientX - offsetX);
+            setPositionY(e.clientY - offsetY);
+        };
+
+        const handleMouseUp = () => {
+            setIsDragging(false);
+        };
+
+        // Listen on document so it works even when mouse leaves the element
+        document.addEventListener('mousemove', handleMouseMove);
+        document.addEventListener('mouseup', handleMouseUp);
+
+        return () => {
+            document.removeEventListener('mousemove', handleMouseMove);
+            document.removeEventListener('mouseup', handleMouseUp);
+        };
+    }, [isDragging, offsetX, offsetY]);
     const popupRef = useRef<HTMLDivElement>(null);
     const closePopup = () => {
         handleClickOutside();
@@ -30,24 +80,42 @@ const Popup: React.FC<PopupProps> = ({ word, definition, mouseX, mouseY, handleC
         setPositionX(x);
         setPositionY(y);
     }, [mouseX, mouseY]);
-    
+    const handleReDefine = () => {
+        browser.runtime.sendMessage({ type: 're-define', word: word, previous_definition: definition });
+    }
+    const handleAudio = () => {
+        if (!audioRef.current) return;
+        audioRef.current.play();
+    }
     return (
         <div
             ref={popupRef}
-            className="underline-none w-100 h-60 fixed bg-black/70 border border-white/10 backdrop-blur-md text-white flex flex-col rounded-md shadow-md"
-            style={{ left: positionX, top: positionY-100 }}
+            className="underline-none w-100 h-60 fixed bg-black/70 border border-white/10 backdrop-blur-md text-white flex flex-col rounded-md shadow-gray-900 shadow-sm"
+            style={{ left: positionX, top: positionY }}
             onClick={(e) => e.stopPropagation()}
         >   
-            <div  className="w-full bg-black/10 flex items-center justify-between px-4 py-2 border-b border-white/10">
+            <div onMouseDown={handleMouseDown} className="w-full cursor-move bg-black/10 flex items-center justify-between px-4 py-2 border-b border-white/10">
                 <div className="text-white text-sm border-0 font-sans font-bold">Rumia</div>
                 <div className="flex items-center gap-2">
-                    {/* <button onClick={handleReDefine}>♲</button> */}
-                    <span onClick={closePopup} className="text-white text-sm border-0 select-none cursor-pointer">x</span>
+                    <span onClick={handleReDefine} className="text-white text-sm border-0 select-none cursor-pointer bg-white/40 rounded-md p-1 w-5 h-5 flex items-center justify-center hover:bg-white/60 transition-all duration-300">
+                        <img src={restart} alt="info" className="w-4 h-4" />
+                    </span>
+                    <span onClick={closePopup} className="text-white text-sm border-0 select-none cursor-pointer bg-white/40 rounded-md p-0 w-5 h-5 flex items-center justify-center hover:bg-red-300 transition-all duration-300">
+                        <img src={cross} alt="info" className="w-5 h-5" />
+                    </span>
                 </div>
                 
             </div>
             <div className="px-4 py-2 gap-2 flex flex-col overflow-y-auto">
-                <div className="text-2xl font-bold no-underline font-sans">{word.charAt(0).toUpperCase() + word.slice(1)}</div>
+                <div className="flex items-center flex-row gap-2">
+                    <div className="text-2xl font-bold no-underline font-sans">{word.charAt(0).toUpperCase() + word.slice(1)}</div>
+                    <div className="flex items-center justify-center pt-1 cursor-pointer">
+                        {audioUrl && <audio src={audioUrl} ref={audioRef} />}
+                        <img onClick={handleAudio} src={audio} alt="" className="w-6 h-6" />
+                    </div>
+                </div>
+                
+
                 <div className="flex flex-col gap-0">
                     <p className="text-white/80 text-sm font-bold font-mono">Definition</p>
                     <p className="text-white/50 text-sm font-sans">{definition? definition.split('Usage:')[0].split('Definition:')[1] : 'Loading...'}</p>
--- entrypoints/background.ts
+++ entrypoints/background.ts
@@ -1,17 +1,18 @@
-import OpenAI from "openai";
-export default defineBackground(() => {
+// import OpenAI from "openai";
+export default defineBackground(async () => {
+  const OpenAI = await import('openai');
   var contextText = '';
-  try{
+  // try{
     browser.contextMenus.create({
       id: 'Rumia',
       title: 'Define (Rumia)',
       contexts: ['selection'],
     });
-  }catch(error){
-    console.error('Error creating context menu', error);
-  }
+  // }catch(error){
+    // console.error('Error creating context menu', error);
+  // }
   
-  browser.contextMenus.onClicked.addListener(async (info, tab) => {
+  browser.contextMenus.onClicked?.addListener(async (info, tab) => {
     const res = await browser.scripting.executeScript({
       target: { tabId: tab?.id as number },
       files: ['content-scripts/getSelection.js']
@@ -21,125 +22,121 @@ export default defineBackground(() => {
     browser.tabs.query({ active: true, currentWindow: true })
     .then(([tab]) => {
       if (tab?.id) {
-        setTimeout(() => {
-          if (tab?.id) {
-            browser.tabs.sendMessage(tab.id, { 
-            type: 'name-studio', 
-              text: info.selectionText,
-              answer: ''
-            });
-          }
-        }, 100);
-        async function getDefinition(word: string) {
-          const groqApiKey = await browser.storage.local.get('groqApiKey').then((result) => {
-            return result.groqApiKey as string;
+        if (tab?.id) {
+          browser.tabs.sendMessage(tab.id, { 
+          type: 'name-studio', 
+            text: info.selectionText,
+            answer: ''
           });
-          const geminiApiKey = await browser.storage.local.get('geminiApiKey').then((result) => {
-            return result.geminiApiKey as string;
-          });
-          var api_key_to_use = '';
-          var base_url = '';
-          var model_to_use = '';
-          if (groqApiKey){
-            api_key_to_use = groqApiKey;
-            base_url = "https://api.groq.com/openai/v1";
-            model_to_use = "openai/gpt-oss-120b";
-          }else{
-            api_key_to_use = '';
-          }
-          const openai = new OpenAI({
-            apiKey: api_key_to_use,
-            baseURL: base_url,
-          });
-          const systemPrompt = await browser.storage.local.get('systemPrompt')
-          var isStreaming = true;
-          try{
-            browser.runtime.onMessage.addListener((message) => {
-              if (message.type === 'stop-streaming') {
-                isStreaming = false
-              }
-            });
-          }catch(error){
-            console.error('Error creating context menu', error);
-          }
-          
-          const simple_context = contextText.split('.').filter((val)=>val.includes(word as string))[0];
-          try{
-            const stream = await openai.responses.create({
-              model: model_to_use,
-              input: `- Provide a definition for the following word, keep it short and concise less than 20 words: ${info.selectionText} 
-              Follow this system prompt: ${systemPrompt.systemPrompt} 
-              Also give another paragraph for how its used in this context: ${simple_context}
-              Use this format:
-              Definition: <definition>
-              Usage: <usage>`,
-              stream: true,
-            });
-            var answer = '';
-            for await (const chunk of stream) {
-              if (!isStreaming) break;
-              if (chunk.type === 'response.output_text.delta') {
-                answer = answer + chunk.delta;
-                if (tab?.id) {
-                  try{
-                    browser.tabs.sendMessage(tab.id, { 
-                      type: 'name-studio-definition', 
-                      text: info.selectionText?.toString() || '',
-                      answer: answer
-                    });
-                  }catch(error){
-                    console.error('Error sending message', error);
-                  }
-                }
-              }
-              if (chunk.type === 'response.output_item.done') {
-                if (tab?.id) {
-                  try{
-                    browser.tabs.sendMessage(tab.id, { 
-                    type: 'name-studio-definition', 
-                    text: info.selectionText,
-                    answer: answer
-                  });
-                  }catch(error){
-                    console.error('Error sending message', error);
-                  }
-                }
-              }
-            }
-          }catch(error : any){
-            console.log('Error creating stream', error);
-            if (tab?.id) {
-              browser.tabs.sendMessage(tab.id, { 
-                type: 'name-studio-definition', 
-                text: info.selectionText,
-                answer: 'Error: ' + error.message
-              });
-            }
-            return;
-          }
         }
-        getDefinition(info.selectionText as string);
-
-            
+        const simple_context = contextText?.split('.').filter((val)=>val.includes(info.selectionText as string))[0];
+        
+        getDefinition(info.selectionText as string, simple_context, tab);
       }
     });
 
-    // browser.runtime.onMessage.addListener(async (message) => {
-    //   if (message.type === 'check-api-key') {
-    //     const groqApiKey = await browser.storage.local.get('groqApiKey').then((result) => {
-    //       return result.groqApiKey as string;
-    //     });
-    //     const openai = new OpenAI({
-    //       apiKey: groqApiKey,
-    //       baseURL: "https://api.groq.com/openai/v1",
-    //     });
-    //     try{
-    //       await openai.models.list();
-    //       return true;
-    //     }catch(error){
-    //       return false;
-    //     }
-    //   }
-    // });
+    browser.runtime.onMessage.addListener(async (message) => {
+      if (message.type === 're-define') {
+        const simple_context = contextText.split('.').filter((val)=>val.includes(message.word as string))[0];
+        browser.tabs.query({ active: true, currentWindow: true })
+        .then(([tab]) => {
+          if (tab?.id) {
+            getDefinition(message.word, simple_context, tab, message.previous_definition);
+          }
+        });
+      }
+    });
   });
+  async function getDefinition(word: string, simple_context: string, tab: any, previous_definition?: string) {
+    const groqApiKey = await browser.storage.local.get('groqApiKey').then((result) => {
+      return result.groqApiKey as string;
+    });
+    const geminiApiKey = await browser.storage.local.get('geminiApiKey').then((result) => {
+      return result.geminiApiKey as string;
+    });
+    var api_key_to_use = '';
+    var base_url = '';
+    var model_to_use = '';
+    if (groqApiKey){
+      api_key_to_use = groqApiKey;
+      base_url = "https://api.groq.com/openai/v1";
+      model_to_use = "openai/gpt-oss-120b";
+    }else{
+      api_key_to_use = '';
+    }
+    const openai = new OpenAI.OpenAI({
+      apiKey: api_key_to_use,
+      baseURL: base_url,
+    });
+    const systemPrompt = await browser.storage.local.get('systemPrompt')
+    var isStreaming = true;
+    try{
+      browser.runtime.onMessage.addListener((message) => {
+        if (message.type === 'stop-streaming') {
+          isStreaming = false
+        }
+      });
+    }catch(error){
+      console.error('Error creating context menu', error);
+    }
+    
+    
+    try{
+      let input = `- Provide a definition for the following word, keep it short and concise less than 20 words: ${word} 
+        Follow this system prompt: ${systemPrompt.systemPrompt} 
+        Also give another paragraph for how its used in this context: ${simple_context}
+        Use this format:
+        Definition: <definition>
+        Usage: <usage>`;
+      if (previous_definition) {
+        input = input + `\n\nPrevious definition was this and user was not satisfied with it: ${previous_definition}`;
+      }
+      const stream = await openai.responses.create({
+        model: model_to_use,
+        input: input,
+        stream: true,
+      });
+      var answer = '';
+      for await (const chunk of stream) {
+        if (!isStreaming) break;
+        if (chunk.type === 'response.output_text.delta') {
+          answer = answer + chunk.delta;
+          if (tab?.id) {
+            try{
+              browser.tabs.sendMessage(tab.id, { 
+                type: 'name-studio-definition', 
+                text: word?.toString() || '',
+                answer: answer
+              });
+            }catch(error){
+              console.error('Error sending message', error);
+            }
+          }
+        }
+        if (chunk.type === 'response.output_item.done') {
+          if (tab?.id) {
+            try{
+              browser.tabs.sendMessage(tab.id, { 
+              type: 'name-studio-definition', 
+              text: word,
+              answer: answer
+            });
+            }catch(error){
+              console.error('Error sending message', error);
+            }
+          }
+        }
+      }
+    }catch(error : any){
+      console.log('Error creating stream', error);
+      if (tab?.id) {
+        browser.tabs.sendMessage(tab.id, { 
+          type: 'name-studio-definition', 
+          text: word,
+          answer: 'Error: ' + error.message
+        });
+      }
+      return;
+    }
+  }
 });
