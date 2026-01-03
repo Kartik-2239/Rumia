2f20b49b4f7108aac523502b6be3bdfe7b0c397c
Kartik <kartikkannan22@gmail.com>
2026-01-03 18:54:06 +0530

    popup ui upgrade

--- entrypoints/popup/App.tsx
+++ entrypoints/popup/App.tsx
@@ -1,10 +1,13 @@
 import "@/assets/tailwind.css";
 import { useState, useEffect } from 'react';
+import eye from '../../assets/eye.svg';
+import eyeOpen from '../../assets/eye-open.svg';
 
 function App() {
   const [geminiApiKey, setGeminiApiKey] = useState('');
   const [groqApiKey, setGroqApiKey] = useState('');
   const [systemPrompt, setSystemPrompt] = useState('');
+  const [groqKeyVisible, setGroqKeyVisible] = useState(false);
   useEffect(() => {
     browser.storage.local.get(['geminiApiKey', 'groqApiKey', 'systemPrompt'], (result) => {
       setGeminiApiKey(result.geminiApiKey as string || '');
@@ -24,42 +27,60 @@ function App() {
     }
   };
 
-  const checkApiKey = async () => {
-    const res = await browser.runtime.sendMessage({ type: 'check-api-key' });
-    if (res) {
-      return true;
-    }
-    return false;
+  const handleSaveConfiguration = () => {
+    browser.storage.local.set({ geminiApiKey: geminiApiKey, groqApiKey: groqApiKey, systemPrompt: systemPrompt });
   };
+
+  const handleToggleGroqKey = () => {
+    setGroqKeyVisible(!groqKeyVisible);
+  };
+
   return (
-    <div className="bg-black/90 w-70 h-100 overflow-y-auto px-2">
-      <div className="flex items-center py-2 px-4 border-b border-white/10">
-        <h1 className='text-2xl text-white font-sans'>
-          Rumia
-        </h1>
+    <div className="bg-black/95 w-70 h-100 overflow-y-auto px-2">
+      <div className="flex flex-col py-2 px-4 border-b border-white/5">
+        <div className="flex items-center gap-2">
+          {/* <img src={icon} alt="logo" className="w-6 h-6 border border-white/10 rounded-md" /> */}
+          <h1 className='text-xl font-bold text-white font-sans'>
+            Rumia
+          </h1>
+        </div>
       </div>
       <div className="flex flex-col gap-2">
         <div className=" rounded-md p-1">
-          {/* <div className="p-2 flex flex-col gap-2">
-            <div className="">
-              <label htmlFor="name" className="text-white font-sans">Gemini Api Key</label>
-              <span className="text-white/30 text-xs"> (get one from <a href="https://ai.studio/" target="_blank" className="text-blue-300 underline">here</a>)</span>
-            </div>
-            <input placeholder="coming soon.." value={geminiApiKey} onChange={(e) => setGeminiApiKey(e.target.value)} onKeyDown={handleGeminiKeyDown} type="password" className="w-full p-2 rounded-md cursor-not-allowed text-white border border-white/50 focus:outline-none" />
-          </div> */}
           <div className="p-2 flex flex-col gap-2">
-            <div className="">
-              <label htmlFor="name" className="text-white font-sans">Groq Api Key</label>
-              <span className="text-white/30 text-xs"> (get one from <a href="https://groq.com/" target="_blank" className="text-blue-300 underline">here</a>)</span>
+            <div className="flex justify-between items-center">
+              <label htmlFor="name" className="text-white font-sans text-xs font-medium">Groq Api Key</label>
+              <span className="flex items-center gap-1"> 
+                <a href="https://groq.com/" target="_blank" className="text-blue-300 font-sans font-extralight text-[10px] hover:underline">
+                  Get Key
+                </a>
+                </span>
             </div>
-            <input value={groqApiKey} type="password" onChange={(e) => setGroqApiKey(e.target.value)} onKeyDown={handleGroqKeyDown} className="w-full p-2 rounded-md text-white border border-white/50 focus:outline-none" />
+            <div>
+              <div className="w-full border border-white/3 rounded-xl bg-white/5 flex items-center gap-1 px-2">
+                {/* <img alt="lock" className="w-4 h-4 cursor-pointer" /> */}
+                <input type={groqKeyVisible ? 'text' : 'password'} value={groqApiKey} onChange={(e) => setGroqApiKey(e.target.value)} onKeyDown={handleGroqKeyDown} className="w-full p-2 rounded-md text-white border-0 bg-transparent border-white/50 focus:outline-none" />
+                <div className="w-5 h-5 cursor-pointer flex items-center justify-center" onClick={handleToggleGroqKey}>
+                  <img alt="eye" src={groqKeyVisible ? eyeOpen : eye} className="w-3 h-3 cursor-pointer filter invert" onClick={handleToggleGroqKey} />
+                </div>
+              </div>
+              {/* <div className="px-1 py-1">
+                <span className="text-white/30 font-extralight text-xs">keys are stored locally on your device</span>
+              </div> */}
+            </div>
+            
           </div>
         </div>
-        <div className=" rounded-md px-1 py-2">
+        <div className="px-1 rounded-md py-2">
           <div className="px-2 flex flex-col gap-2">
-            <label htmlFor="name" className="text-white font-sans">System Prompt</label>
-            <textarea rows={5} placeholder="click enter to save system prompt" value={systemPrompt} onChange={(e) => setSystemPrompt(e.target.value)} onKeyDown={handleSystemPromptKeyDown} className="w-full p-2 rounded-md text-white border border-white/50 focus:outline-none" />
+            <label htmlFor="name" className="text-white font-sans text-xs font-medium">System Prompt</label>
+            <div className="w-full border border-white/3 rounded-xl bg-white/5 flex items-center gap-1 px-1 text-white">
+              <textarea rows={7} placeholder="enter your system prompt here ..." value={systemPrompt} onChange={(e) => setSystemPrompt(e.target.value)} onKeyDown={handleSystemPromptKeyDown} className="w-full p-2 rounded-md focus:outline-none font-extralight font-sans resize-none" />
+            </div>
           </div>
+        </div>
+        <div className="py-2 px-3">
+            <button onClick={handleSaveConfiguration} className="w-full border border-white/3 bg-white/5 text-white/50 px-4 py-2 rounded-xl hover:bg-white/15 cursor-pointer transition-all duration-300">Save Configuration</button>
         </div>
       </div>
 
